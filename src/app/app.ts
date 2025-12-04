36dd6a57be3df8784e55c4ad97576d44450c18ef
Nihad <nihad.nemetli@gmail.com>
2025-12-04 12:17:13 +0400

    new: header folder created and files moved

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',

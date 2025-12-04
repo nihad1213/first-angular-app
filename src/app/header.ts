1351cb18757f60bdae8796c0a7bc1c4000296254
Nihad <nihad.nemetli@gmail.com>
2025-12-04 11:55:01 +0400

    new: assets folder created

--- src/app/header.ts
+++ src/app/header.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.html',
+    styleUrl: './header.css'
 })
 export class HeaderComponent {}

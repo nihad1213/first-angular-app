4543b1a9641680ad88a59ac14f49a5052c8291f2
Nihad <nihad.nemetli@gmail.com>
2025-12-04 12:29:24 +0400

    error fixed

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-import { HeaderComponent } from './app/header/header';
-import { UserComponent } from './app/user/user';
-
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
-
-bootstrapApplication(HeaderComponent, appConfig)
-  .catch((err) => console.error(err));
-
-bootstrapApplication(UserComponent, appConfig)
   .catch((err) => console.error(err));

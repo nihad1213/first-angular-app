761e0ceb09805df805aa7427075be501e1555ce0
Nihad <nihad.nemetli@gmail.com>
2025-12-04 11:40:27 +0400

    add: Header added to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { HeaderComponent } from './app/header';
+
 bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(HeaderComponent, appConfig)
   .catch((err) => console.error(err));

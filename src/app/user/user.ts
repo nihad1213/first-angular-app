09e9f6456bb1a73a1afb6a84872982cd62547fe3
Nihad <nihad.nemetli@gmail.com>
2025-12-05 15:52:40 +0400

    event listener added

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -17,4 +17,8 @@ export class UserComponent {
   get imagePath(): string {
     return 'assets/users/' + this.selectedUser.avatar;
   }
+
+  onSelectUser(): void {
+    alert(`User Selected: ${this.selectedUser.name}`);
+  }
 }

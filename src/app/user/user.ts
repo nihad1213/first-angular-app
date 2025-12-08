0413fdd67094505871ded04457d0cfad4abca0ab
Nihad <nihad.nemetli@gmail.com>
2025-12-08 14:53:39 +0400

    del: uneceassary part deleted

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,16 +8,9 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); 
-
-
-  // get imagePath(): string {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  
 
   onSelectUser(): void {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
   }
 }
